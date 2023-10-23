27fbaaef5fcc8f7bce0b522052feaa1d8a6c74cb
Asher <ashersam116@gmail.com>
2023-10-23 02:25:23 -0900

    login page design done!

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,52 @@
 import "./login.scss";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
 
 const Login = () => {
-  return <div>Login Page</div>;
+  return (
+    <div className="login pd">
+      <div className="loginNavbar">
+        <div className="l-title">Creative Finance</div>
+      </div>
+      <div className="loginBody">
+        <div className="b-illustration"></div>
+        <div className="b-form">
+          <div className="form-title">
+            <div>Welcome Back!</div>
+            <p>Login to continue</p>
+          </div>
+
+          <form action="">
+            <div className="inputBox">
+              <PersonOutlineIcon className="icon" />
+              <input type="text" name="" id="" placeholder="Enter Username" />
+            </div>
+
+            <div className="inputBox">
+              <VpnKeyIcon className="icon" />
+              <input
+                type="password"
+                name=""
+                id=""
+                placeholder="Enter Password"
+              />
+            </div>
+          </form>
+          <div className="btn-box">
+            <button>LOGIN</button>
+          </div>
+        </div>
+      </div>
+      <div className="loginFooter">
+        <div className="l-footer-detail">
+          Developed by
+          <a target="_blank" href="https://t.me/ashermuzic">
+            Asher
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
